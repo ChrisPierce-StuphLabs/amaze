b8b49a6dc107881e25fe507cd77a3b23e0502035
Chris Pierce <crizpiz@gmail.com>
2015-08-08 13:33:51 -0500

    added background and jump function

--- script.js
+++ script.js
@@ -5,26 +5,36 @@ $(document).ready(function() {
 		
 			// Left arrow key pressed
 			case 37:
-				$('img').animate({left: "-=10px"}, 'fast');
+				$('img').animate({left: "-=40px"}, 'fast');
 				break;
 				
 				
 			// Up Arrow Pressed
 			case 38:
-				$('img').animate({top: "-=10px"}, 'fast');
+				$('img').animate({top: "-=40px"}, 'fast');
 				break;
 				
 				
 			// Right Arrow Pressed
 			case 39:
-				$('img').animate({left: "+=10px"}, 'fast');
+				$('img').animate({left: "+=40px"}, 'fast');
 				break;
-				
+					
 				
 			// Down Arrow Pressed
 			case 40:
-				$('img').animate({top: "+=10px"}, 'fast');
-				break;
+				$('img').animate({top: "+=40px"}, 'fast');
+				break;			
+				
+
+
+			// Space Pressed
+			case 32:
+				$('img').animate({top: "-=40px"}, 'fast');
+				$('img').animate({top: "-=40px"}, 'fast');
+				$('img').animate({top: "+=40px"}, 'fast');
+				$('img').animate({top: "+=40px"}, 'fast');
+				break;  
 		}
 	});
 });
